b1096f3fa5f074c266b2ef0a5236c982bb14af32
Zeeshan Meghji <zeeshan_meghji@hotmail.com>
2019-09-07 18:38:22 -0400

    implemented ioptions for configuration

---
+++ DotNetCoreConfiguration/Options/DataSourceOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreConfiguration.Options
+{
+    public class DataSourceOptions
+    {
+        public string Classification { get; set; }
+        public string Type { get; set; }
+    }
+}
--- DotNetCoreConfiguration/Startup.cs
+++ DotNetCoreConfiguration/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DotNetCoreConfiguration.Options;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -27,6 +28,7 @@ namespace DotNetCoreConfiguration
 
             services.AddControllersWithViews();
             services.AddRazorPages();
+            services.Configure<DataSourceOptions>(Configuration.GetSection("DataSource"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
