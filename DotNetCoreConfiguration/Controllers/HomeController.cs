bf85cd88185087bc7f96185e8f0183da271a09d4
Zeeshan Meghji <zeeshan_meghji@hotmail.com>
2019-09-07 18:23:23 -0400

    initial commit. Implemented xml and ini configuration sources

---
+++ DotNetCoreConfiguration/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using DotNetCoreConfiguration.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace DotNetCoreConfiguration.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration configuration;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            this.configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Value1 = configuration.GetValue<string>("section:section0:key:key0");
+            ViewBag.Value2 = configuration.GetValue<string>("section:section0:key:key1");
+
+            ViewBag.Value3 = configuration.GetValue<string>("section:section1:key:key0");
+            ViewBag.Value4 = configuration.GetValue<string>("section:section1:key:key1");
+
+            ViewBag.DefaultFramework = configuration.GetValue<string>("section:appSettings:key:defaultFramework");
+            ViewBag.DefaultLanguage = configuration.GetValue<string>("section:appSettings:key:defaultLanguage");
+
+            ViewBag.IniValue1 = configuration.GetValue<string>("sectionX:key0");
+            ViewBag.IniValue2 = configuration.GetValue<string>("sectionX:key1");
+            ViewBag.IniValue3 = configuration.GetValue<string>("sectionY:subsection:key");
+            ViewBag.IniValue4 = configuration.GetValue<string>("sectionZ:subsection0:key");
+            ViewBag.IniValue5 = configuration.GetValue<string>("sectionZ:subsection1:key");
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DotNetCoreConfiguration/Program.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace DotNetCoreConfiguration
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var x = Path.Combine(Directory.GetCurrentDirectory(), "web.config");
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                })
+                .ConfigureAppConfiguration( (hostBuilderContext,configurationBuilder) =>
+                {
+                    configurationBuilder.AddXmlFile(Path.Combine(Directory.GetCurrentDirectory() ,"config.xml"));
+                    configurationBuilder.AddIniFile(Path.Combine(Directory.GetCurrentDirectory(), "config.ini"));
+
+                }
+                );
+    }
+}
